8d06a74b276342705660f03a3e917bccf56acc10
Jonas Bernoulli <jonas@bernoul.li>
2017-06-28 14:29:19 +0200

    More fixes before adding to Melpa (#2)
    * Fix indentation, remove unnecessary used of progn
    
    * Shorten long lines
    
    Also improve `letterbox-mode's doc-string a bit and use `cadr'
    instead of `car' and `cdr' (both changes on shorted lines).
    
    * Cosmetic changes to letterbox-add
    
    * Adding an element to an empty list using `add-to-list' is just
      the same as doing it for a non-empty list.
    
    * Use `unless' instead of `if' and `not'.
    
    * Use local variable in letterbox-toggle

--- letterbox-mode.el
+++ letterbox-mode.el
@@ -25,34 +25,49 @@
 ;;; Commentary:
 
 ;; Letterbox-mode is a simple minor mode to add letterboxing to sensitive text.
-;; Select a region of text you want to censor and call M-x letterbox-add, the selected region will be letterboxed/censored.
-;; Call M-x letterbox-remove to remove all letterboxes, or M-x to toggle the letterboxes (without removing them).
-;; Some use cases:
 ;;
-;;	 This is a buffer with some sensitive information. 123456 is the password for my bank account, and qwerty is my account name. please don't read it.
+;; Select a region of text you want to censor and call M-x
+;; letterbox-add, the selected region will be letterboxed/censored.
+;; Call M-x letterbox-remove to remove all letterboxes, or M-x to
+;; toggle the letterboxes (without removing them).  Some use cases:
 ;;
-;; Select "123456" and call letterbox-add, you will see that part letterboxed:
+;;	 This is a buffer with some sensitive information. 123456 is
+;;	 the password for my bank account, and qwerty is my account
+;;	 name. please don't read it.
 ;;
-;;	 This is a buffer with some sensitive information. ██████ is the password for my bank account, and qwerty is my account name. please don't read it.
+;; Select "123456" and call letterbox-add, you will see that part
+;; letterboxed:
 ;;
-;; Select "qwerty" and call letterbox-add again, you will see that part letterboxed as well:
+;;	 This is a buffer with some sensitive information. ██████ is
+;;	 the password for my bank account, and qwerty is my account
+;;	 name. please don't read it.
 ;;
-;;	 This is a buffer with some sensitive information. ██████ is the password for my bank account, and ██████ is my account name. please don't read it.
+;; Select "qwerty" and call letterbox-add again, you will see that
+;; part letterboxed as well:
 ;;
-;; Call letterbox-toggle to hide/show the sensitive text, or letterbox-remove to remove them.
+;;	 This is a buffer with some sensitive information. ██████ is
+;;	 the password for my bank account, and ██████ is my account
+;;	 name. please don't read it.
+;;
+;; Call letterbox-toggle to hide/show the sensitive text, or
+;; letterbox-remove to remove them.
 
 ;;; Code:
 
 ;;;###autoload
 (define-minor-mode letterbox-mode
-  "Letterbox text in current buffer, select region and press <C-x l> to letterbox, press <C-x t> to toggle letterbox visibility, press <C-x d> to remove all active letterboxes."
+  "Mode for hiding sensitive text on a buffer.
+
+After activating this mode in current buffer, select region and
+press <C-x l> to letterbox, press <C-x t> to toggle letterbox
+visibility, press <C-x d> to remove all active letterboxes."
   :init-value nil
   :lighter " Letterbox"
   :keymap (let ((map (make-sparse-keymap)))
-			(define-key map (kbd "C-c a") 'letterbox-add)
-			(define-key map (kbd "C-c t") 'letterbox-toggle)
-			(define-key map (kbd "C-c r") 'letterbox-remove)
-			map)
+            (define-key map (kbd "C-c a") 'letterbox-add)
+            (define-key map (kbd "C-c t") 'letterbox-toggle)
+            (define-key map (kbd "C-c r") 'letterbox-remove)
+            map)
   (setq letterbox-current-text nil)
   (setq letterbox-is-visible t)
   (remove-overlays (point-min) (point-max) 'category 'letterbox))
@@ -65,7 +80,8 @@
   :group 'convenience)
 
 (defface letterbox-face
-  '(t (:background (face-attribute 'default :foreground) :foreground (face-attribute 'default :foreground)))
+  '(t (:background (face-attribute 'default :foreground)
+       :foreground (face-attribute 'default :foreground)))
   "Letterbox mode face."
   :group 'letterbox)
 
@@ -73,14 +89,14 @@
 (defun letterbox-add()
   (interactive)
   (if (and mark-active (not (= 0 (- (region-beginning) (region-end)))))
-	  (progn (if (equal nil letterbox-current-text)
-				 (setq letterbox-current-text (list (list (region-beginning) (region-end))))
-			   (add-to-list 'letterbox-current-text (list (region-beginning) (region-end)) t))
-			 (deactivate-mark)
-			 (if (not letterbox-is-visible)
-				 (letterbox-toggle))
-			 (letterbox-refresh-overlays))
-	(message "No region active to add text to a letterbox.")))
+      (progn (add-to-list 'letterbox-current-text
+                          (list (region-beginning) (region-end))
+                          t)
+             (deactivate-mark)
+             (unless letterbox-is-visible
+               (letterbox-toggle))
+             (letterbox-refresh-overlays))
+    (message "No region active to add text to a letterbox.")))
 
 ;;;###autoload
 (defun letterbox-remove()
@@ -94,17 +110,21 @@
   (interactive)
   (setq letterbox-is-visible (not letterbox-is-visible))
   (if letterbox-is-visible
-	  (message "Letterbox enabled")
-	(message "Letterbox disabled"))
+      (message "Letterbox enabled")
+    (message "Letterbox disabled"))
   (letterbox-refresh-overlays))
 
 (defun letterbox-refresh-overlays()
   (if (and letterbox-is-visible letterbox-current-text)
-	  (progn (dolist (letterbox-list letterbox-current-text)
-			   (progn (setq overlay-helper (make-overlay (car letterbox-list) (car (cdr letterbox-list))))
-					  (overlay-put overlay-helper 'category 'letterbox)
-					  (overlay-put overlay-helper 'face 'letterbox-face))))
-	(remove-overlays (point-min) (point-max) 'category 'letterbox)))
+      (dolist (letterbox-list letterbox-current-text)
+        (let ((ov (make-overlay (car letterbox-list)
+                                (cadr letterbox-list))))
+          (overlay-put ov 'category 'letterbox)
+          (overlay-put ov 'face 'letterbox-face)))
+    (remove-overlays (point-min) (point-max) 'category 'letterbox)))
 
 (provide 'letterbox-mode)
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
 ;;; letterbox-mode.el ends here
