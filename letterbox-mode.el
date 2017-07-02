88c67a51d67216d569a28e8423200883fde096dd
Pacha <f.leboran@gmail.com>
2017-07-01 22:25:41 -0300

    Update letterbox-mode.el

--- letterbox-mode.el
+++ letterbox-mode.el
@@ -7,6 +7,7 @@
 ;; URL: http://www.github.com/pacha64/letterbox-mode
 ;; Package-Requires: ((emacs "24.3"))
 
+;; Copyright (C) 2017 Fernando Leboran
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
